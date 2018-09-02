5b99fc43581626ef035f8424f6a94189a820f971
Sanjay <sanjaykr.kundu@gmail.com>
2018-09-02 17:56:58 +0530

    init

---
+++ src/main/java/science/revenswan/cms/controller/UserController.java
@@ -0,0 +1,21 @@
+package science.revenswan.cms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import science.revenswan.cms.model.User;
+
+@Controller
+public class UserController{
+
+    @RequestMapping("/")
+    @ResponseBody
+    public User test(){
+        User user = new User("sanjaykr.kundu@gmail.com","pass");
+ 
+        return user;
+    }
+
+
+}
---
+++ src/main/java/science/revenswan/cms/model/User.java
@@ -0,0 +1,18 @@
+package science.revenswan.cms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
+public class User{
+    private String email;
+    private String password;
+    
+
+
+}
